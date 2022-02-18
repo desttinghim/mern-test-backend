650b18f0b9b6c7951eb1e7ce61cc6360b95ef200
Louis Pearson <louispearson@librem.one>
2022-02-17 18:44:52 -0700

    Don't allow multiple accounts with the same email

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -20,6 +20,13 @@ const getUsers = (req, res, next) => {
 const signup = (req, res, next) => {
   const { name, email, password } = req.body;
 
+  const hasUser = DUMMY_USERS.find((u) => u.email === email);
+  if (hasUser) {
+    return next(
+      new HttpError("Could not create user, email already exists.", 422)
+    );
+  }
+
   const id = uuidv4();
 
   DUMMY_USERS.push({
