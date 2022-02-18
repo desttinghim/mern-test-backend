61cf7488202803050636c1bcdb014f7b1df144b9
Louis Pearson <louispearson@librem.one>
2022-02-17 23:22:14 -0700

    Add express validator

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,4 +1,5 @@
 const { v4: uuidv4 } = require("uuid");
+const { validationResult } = require("express-validator");
 
 const HttpError = require("../models/http-error");
 
@@ -12,12 +13,24 @@ const DUMMY_USERS = [
 ];
 
 const getUsers = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors);
+    throw new HttpError("Invalid inputs passed, please check your data.", 422);
+  }
+
   const users = DUMMY_USERS.map((user) => ({ id: user.id, name: user.name }));
 
   res.status(200).json({ users });
 };
 
 const signup = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors);
+    throw new HttpError("Invalid inputs passed, please check your data.", 422);
+  }
+
   const { name, email, password } = req.body;
 
   const hasUser = DUMMY_USERS.find((u) => u.email === email);
--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { check } = require("express-validator");
 
 const usersController = require("../controllers/users-controller");
 
@@ -6,8 +7,20 @@ const router = express.Router();
 
 router.get("/", usersController.getUsers);
 
-router.post("/signup", usersController.signup);
+router.post(
+  "/signup",
+  [
+    check("name").not().isEmpty(),
+    check("email").isEmail(),
+    check("password").isLength({ min: 5 }),
+  ],
+  usersController.signup
+);
 
-router.post("/login", usersController.login);
+router.post(
+  "/login",
+  [check("email").isEmail(), check("password").isLength({ min: 5 })],
+  usersController.login
+);
 
 module.exports = router;
