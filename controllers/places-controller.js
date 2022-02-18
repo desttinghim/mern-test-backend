c1819bcb059e8b1eb4bf47e1186cc5d5dbf24f97
Louis Pearson <louispearson@librem.one>
2022-02-17 18:15:56 -0700

    Follow lecture for update and delete places

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -2,7 +2,7 @@ const { v4: uuidv4 } = require("uuid");
 
 const HttpError = require("../models/http-error");
 
-const DUMMY_PLACES = [
+let DUMMY_PLACES = [
   {
     id: "p1",
     title: "Empire State Building",
@@ -55,40 +55,31 @@ const createPlace = (req, res, next) => {
   res.status(201).json({ place: createdPlace });
 };
 
-const updatePlaceById = (req, res, next) => {
+const updatePlace = (req, res, next) => {
+  const { title, description } = req.body;
   const placeId = req.params.pid;
 
-  const updatedPlace = DUMMY_PLACES.find((p) => p.id === placeId);
-
-  if (!updatedPlace) {
+  const placeIndex = DUMMY_PLACES.findIndex((p) => p.id === placeId);
+  if (placeIndex < 0)
     return next(new HttpError("Could not find place for provided id.", 404));
-  }
 
-  const { title, description, coordinates, address, creator } = req.body;
-
+  const updatedPlace = { ...DUMMY_PLACES.find((p) => p.id === placeId) };
   updatedPlace.title = title;
   updatedPlace.description = description;
-  updatedPlace.location = coordinates;
-  updatedPlace.address = address;
-  updatedPlace.creator = creator;
+
+  DUMMY_PLACES[placeIndex] = updatedPlace;
 
   res.status(200).json({ place: updatedPlace });
 };
 
-const deletePlaceById = (req, res, next) => {
+const deletePlace = (req, res, next) => {
   const placeId = req.params.pid;
-
-  const deleteIndex = DUMMY_PLACES.findIndex((p) => p.id === placeId);
-
-  if (deleteIndex < 0) {
-    return next(new HttpError("Could not find place for provided id.", 404));
-  }
-
-  res.status(200).json({ place: DUMMY_PLACES.splice(deleteIndex) });
+  DUMMY_PLACES = DUMMY_PLACES.filter((p) => p.id !== placeId);
+  res.status(200).json({ message: "Deleted place" });
 };
 
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
 exports.createPlace = createPlace;
-exports.updatePlaceById = updatePlaceById;
-exports.deletePlaceById = deletePlaceById;
+exports.updatePlace = updatePlace;
+exports.deletePlace = deletePlace;
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -10,8 +10,8 @@ router.get("/user/:uid", placesController.getPlaceByUserId);
 
 router.post("/", placesController.createPlace);
 
-router.patch("/:pid", placesController.updatePlaceById);
+router.patch("/:pid", placesController.updatePlace);
 
-router.delete("/:pid", placesController.deletePlaceById);
+router.delete("/:pid", placesController.deletePlace);
 
 module.exports = router;
