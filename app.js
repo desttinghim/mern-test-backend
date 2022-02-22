74bb298075a237ddf7c7ada6549cb517f5234d2b
Louis Pearson <louispearson@librem.one>
2022-02-22 12:50:47 -0700

    Add Cors headers

--- app.js
+++ app.js
@@ -10,6 +10,17 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+
+  next();
+});
+
 app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
 
@@ -30,7 +41,7 @@ const mongoPasswd = require("./secrets.js").mongoPasswd;
 
 mongoose
   .connect(
-    `mongodb+srv://lpearson:${mongoPasswd}@cluster0.lol2s.mongodb.net/places?retryWrites=true&w=majority`
+    `mongodb+srv://lpearson:${mongoPasswd}@cluster0.lol2s.mongodb.net/mern?retryWrites=true&w=majority`
   )
   .then(() => {
     app.listen(5000);
